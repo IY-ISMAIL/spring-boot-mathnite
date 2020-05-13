534a132e2e48b276229a322819bfb2f524c585ca
Ismail Younes <isma.younes1998@gmail.com>
2020-05-12 21:19:14 -0300

    Inicio do projeto com spring boot

---
+++ src/main/java/br/com/mathnite/api/MathniteApplication.java
@@ -0,0 +1,13 @@
+package br.com.mathnite.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathniteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MathniteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/mathnite/api/MathniteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mathnite.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathniteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
